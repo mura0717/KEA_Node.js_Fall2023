55279b63f5f83e5dcf8d6ee468195d7782c64aea
mura0717 <mura0717@stud.kea.dk>
2023-08-22 12:41:04 +0200

    Objects Exercises solved.

--- 00_Miscellaneous/01._Intro_Exercises/3.Objects.js
+++ 00_Miscellaneous/01._Intro_Exercises/3.Objects.js
@@ -7,21 +7,32 @@ const myObj = { message: "Hello, earthling! I bring peace." };
 
 // Log the message 
 
-console.log(myObj);
+console.log(myObj.message);
 
 // --------------------------------------
 // Exercise 2 - Defining an object. 
 
 // Create an object that has your name and age. 
 
+const person = {
+    firstName: "Kaan",
+    age: 39
+}
+
+console.log(person)
 
 // --------------------------------------
 // Exercise 3 - Add a property 
 
 const stackOverflow = {};
 
+stackOverflow.message = "Error message";
+
 // make a rule called isAllowed and let the value be true
 
+stackOverflow.isAllowed = true;
+
+console.log(stackOverflow);
 // --------------------------------------
 // Exercise 4 - Remove a property 
 
@@ -29,5 +40,7 @@ const thisSong = { description: "The best song in the world." };
 
 // remove the property "description" and add a property called "about" that should say "Just a tribute." 
 
-
+delete thisSong.description;
+thisSong.about = "Just a tribute.";
+console.log(thisSong);
 // --------------------------------------
