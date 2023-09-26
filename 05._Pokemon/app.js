23a59e3a3f78f93c849178bb16421d8f864b42ec
mura0717 <mura0717@stud.kea.dk>
2023-09-26 11:48:51 +0200

    pokemon battle finished

--- 05._Pokemon/app.js
+++ 05._Pokemon/app.js
@@ -5,6 +5,7 @@ const app = express();
 app.use(express.static("public"));
 
 import path from "path" // because __dirname is not defined in ES Module scope.
+import { randomIntFromInterval } from "./util/randomUtil.js";
 
 //console.log(path.resolve("./public/frontpage.html"))
 //console.log(path.resolve("./")) //gives absolute path
@@ -29,7 +30,35 @@ app.get("/contact", (req, res) => {
 let currentPokemon;
 
 app.get("/battlepokemon", (req, res) => {
-    
+
+    if (!currentPokemon || currentPokemon.strength <= 1){
+        const randomPokemonId = randomIntFromInterval(1, 151);
+        fetch(`https://pokeapi.co/api/v2/pokemon/${randomPokemonId}`)
+        .then(((response) => {
+            if (!response.ok) {
+            throw new Error("Error getting a pokemon.");
+            }
+            return response.json();
+        }))
+        .then((result) => {
+            const name = result.name.charAt(0).toUpperCase() + result.name.slice(1);
+            const imageURL = result.sprites.other.home.front_default;
+            const strength = randomIntFromInterval(1, 10);
+
+            currentPokemon = {
+                name,
+                imageURL,
+                strength
+            };
+            
+            res.send({ data: currentPokemon});
+        })
+    }
+    else {
+        currentPokemon.strength--;
+        res.send({ data: currentPokemon});
+    }
+        
 })
 
 
--- 05._Pokemon/public/battle/battle.js
+++ 05._Pokemon/public/battle/battle.js
@@ -1,10 +1,14 @@
-console.log("hello")
+fetch("/battlepokemon")
+.then((response) => response.json())
+.then((result) => {
+    const nameHeader = document.getElementById("pokemon-name");
+    nameHeader.innerText = result.data.name;
 
-// task: create a fetch to /battlepokemon
+    const strengthHeader = document.getElementById("pokemon-strength");
+    strengthHeader.innerText = "Remaining Strength: " + result.data.strength;
 
-    fetch("/battlepokemon")
-    .then((response) => response.json)
-    .then((result) => {
-        console.log(result); 
-    });
-  
+    const imageWrapperDiv = document.getElementById("image-wrapper");
+    imageWrapperDiv.innerHTML = `
+    <img id="pokemon-image" src= ${result.data.imageURL}>`
+    
+});
