1f47f139e24ea93a8eaef3d808f3ae4f26362791
mura0717 <mura0717@stud.kea.dk>
2023-09-28 18:17:22 +0200

    content done html sidebar fix & general divs made ready

--- 06._Mandatory_I/public/sidebar/sidebar.js
+++ 06._Mandatory_I/public/sidebar/sidebar.js
@@ -1,9 +1,9 @@
-    // Fetch sidebar
-    fetch('../../sidebar/sidebar.html')
-        .then(response => response.text())
-        .then(result => {
-            document.getElementById('sidebar-container').innerHTML = result
-        })
-        .catch(error => {
-            console.error('Error loading sidebar:', error);
-        });
+// Fetch sidebar
+fetch("../../sidebar/sidebar.html")
+  .then((response) => response.text())
+  .then((result) => {
+    document.getElementById("sidebar-container").innerHTML = result;
+  })
+  .catch((error) => {
+    console.error("Error loading sidebar:", error);
+  });
