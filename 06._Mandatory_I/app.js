fe93c8b9f438aed36552e3a87a4d1fbf10ee88b5
mura0717 <mura0717@stud.kea.dk>
2023-10-02 15:13:24 +0200

    up to week 5 done & login started

--- 06._Mandatory_I/app.js
+++ 06._Mandatory_I/app.js
@@ -62,6 +62,45 @@ app.get("/loops_js", (req, res) => {
     res.sendFile(__dirname + "/public/week3/loops_js/loops_js.html")
 })
 
+app.get("/servingExpress_inHtml", (req, res) => {
+    res.sendFile(__dirname + "/public/week3/servingExpress_inHtml/servingExpress_inHtml.html")
+})
+
+app.get("/servingExpress_inHtml", (req, res) => {
+    res.sendFile(__dirname + "/public/week3/servingExpress_inHtml/servingExpress_inHtml.html")
+})
+
+//Week4
+app.get("/dates_times", (req, res) => {
+    res.sendFile(__dirname + "/public/week4/dates_times/dates_times.html")
+})
+
+app.get("/fetch_basics", (req, res) => {
+    res.sendFile(__dirname + "/public/week4/fetch_basics/fetch_basics.html")
+})
+
+app.get("/deployment", (req, res) => {
+    res.sendFile(__dirname + "/public/week4/deployment/deployment.html")
+})
+
+//Week5
+app.get("/commonJS_ESmodules", (req, res) => {
+    res.sendFile(__dirname + "/public/week5/commonJS_ESmodules/commonJS_ESmodules.html")
+})
+
+app.get("/import_export_modules", (req, res) => {
+    res.sendFile(__dirname + "/public/week5/import_export_modules/import_export_modules.html")
+})
+
+app.get("/xss", (req, res) => {
+    res.sendFile(__dirname + "/public/week5/xss/xss.html")
+})
+
+//Login
+ app.get("/login", (req, res) => {
+    res.sendFile(__dirname + "/public/login/login.html")
+}) 
+
 //connection
 const PORT = 8080;
 app.listen(PORT, (error) => {
---
+++ 06._Mandatory_I/public/login/login.js
@@ -0,0 +1,8 @@
+
+    const loginButton = document.getElementById("submit-button");
+    const sidebarLoginButton = document.getElementById("sidebar-login-button")
+
+    loginButton.addEventListener("click", function (event) {
+        window.location.href = "/";
+    });
+
