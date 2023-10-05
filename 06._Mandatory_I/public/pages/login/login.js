70aed10cb4d4c409f6efd0bde6db2a186a996990
mura0717 <mura0717@stud.kea.dk>
2023-10-05 16:10:48 +0200

    login & signup update but not finished

--- 06._Mandatory_I/public/pages/login/login.js
+++ 06._Mandatory_I/public/pages/login/login.js
@@ -3,17 +3,25 @@ const submitLoginForm = document.getElementById("login-form")
 const savedUsername = sessionStorage.getItem("username");
 const savedPassword = sessionStorage.getItem("password");
 
+//auto-refill username & password from sign-up
+document.getElementById("input-username").value = savedUsername;
+document.getElementById("input-password").value = savedPassword;
+
 submitLoginForm.addEventListener("submit", (event) => {
     event.preventDefault();
-    let username = document.getElementById("inputUsername").value
-    let password = document.getElementById("inputPassword").value
-    username = savedUsername;
-    password = savedPassword;
-    if (username == savedPassword && password == savedPassword){
+    
+    const givenUsername = document.getElementById("input-username").value;
+    const givenPassword = document.getElementById("input-uassword").value;
+
+    if (savedPassword == givenUsername && savedPassword == givenPassword){
         window.location.href = ("/admin");
     } else {
-        console.log("Error: Wrong username or password.");
-        return;
+        const errorText = document.getElementById("message")
+        errorText.innerHTML = `
+        Wrong username or password. <br> 
+        The saved data from session storage is ${savedUsername} and ${savedPassword}. <br> 
+        Use this data to log in.
+        `
     }
 })
 
--- 06._Mandatory_I/public/pages/signup/signup.js
+++ 06._Mandatory_I/public/pages/signup/signup.js
@@ -2,13 +2,13 @@ const submitSignUpForm = document.getElementById("sign-up-form")
 
 submitSignUpForm.addEventListener("submit", (event) => {
     event.preventDefault();
-    const inputUsername = document.getElementById("inputUsername").value;
-    const inputPassword = document.getElementById("inputPassword").value;
+    const inputUsername = document.getElementById("input-username").value;
+    const inputPassword = document.getElementById("input-password").value;
     sessionStorage.setItem("username", inputUsername);
     sessionStorage.setItem("password", inputPassword);
     if (!inputUsername && !inputUsername) {
-        alert("Error: Form can't be empty.");
-        console.log("Error: Form can't be empty.");
+        const errorText = document.getElementById("message")
+        errorText.innerHTML = "Form can't be submitted empty."
     } else {
         window.location.href = ("/login");
     }
