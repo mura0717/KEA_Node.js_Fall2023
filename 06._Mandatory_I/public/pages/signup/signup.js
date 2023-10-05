c7bd08e05e4bfc91d8781955f975cbe3aa9a542d
mura0717 <mura0717@stud.kea.dk>
2023-10-05 08:29:12 +0200

    admin bg styling update

--- 06._Mandatory_I/public/pages/signup/signup.js
+++ 06._Mandatory_I/public/pages/signup/signup.js
@@ -1,15 +1,15 @@
-const submitSignUpForm = document.getElementById("submit_form")
+const submitSignUpForm = document.getElementById("sign-up-form")
 
 submitSignUpForm.addEventListener("submit", () => {
     const savedUsername = document.getElementById("inputUsername").value;
     const savedPassword = document.getElementById("inputPassword").value;
-    if (!savedUsername || !savedPassword){
-        console.log("Error: Failed to fill out the form.");
+    if (!savedUsername && !savedPassword){
+        console.log("Error: Form can't be empty.");
         return;
     } else {
         localStorage.setItem("username", savedUsername);
         localStorage.setItem("password", savedPassword);
-        window.location.href("/login");
+        window.location.href = ("/login");
     }
 })
 
