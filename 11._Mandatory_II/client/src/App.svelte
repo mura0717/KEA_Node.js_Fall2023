5324567663f02fb5919e5126520a0737199eb124
mura0717 <mura0717@stud.kea.dk>
2023-10-27 19:36:13 +0200

    mandatory II client set up & front end tailwind styling almost done

---
+++ 11._Mandatory_II/client/src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { Router, Link, Route } from "svelte-navigator";
+  import Home from "./pages/LoginHome/LoginHome.svelte";
+  import NavBar from './components/NavBar.svelte';
+</script>
+
+<Router>
+
+  <NavBar {Link}/>
+
+  <div>
+    <Route path="/" component={Home}></Route>
+  </div>
+
+</Router>
+
+<main>
+
+
+</main>
---
+++ 11._Mandatory_II/client/src/components/NavBar.svelte
@@ -0,0 +1,25 @@
+<script>
+  import NavItemSpecial from "./NavItemSpecial.svelte";
+  export let Link;
+</script>
+
+<nav
+  class="relative flex w-full flex-wrap items-center justify-between bg-[#FBFBFB] py-2 text-neutral-500 shadow-lg hover:text-neutral-700 focus:text-neutral-700 dark:bg-neutral-600 lg:py-1"
+  data-te-navbar-ref
+>
+  <div class="flex w-full flex-wrap items-center justify-between px-3">
+    <!-- Home Icon -->
+    <NavItemSpecial to="/" Link={Link} text="" />
+
+    <!-- Sign up -->
+      <button
+        type="button"
+        data-te-ripple-init
+        data-te-ripple-color="light"
+        class="mr-3 font-semibold inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] motion-reduce:transition-none dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+      >
+        Sign up
+      </button>
+
+  </div>
+</nav>
