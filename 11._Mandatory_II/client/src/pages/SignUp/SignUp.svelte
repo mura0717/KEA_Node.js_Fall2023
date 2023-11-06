b773ea317fa76c77b426d5d066b26c0f0f58fe50
mura0717 <mura0717@stud.kea.dk>
2023-11-06 15:46:58 +0100

    mysql test done

--- 11._Mandatory_II/client/src/pages/SignUp/SignUp.svelte
+++ 11._Mandatory_II/client/src/pages/SignUp/SignUp.svelte
@@ -1,3 +1,35 @@
+<script>
+  let name= '';
+  let email= '';
+  let password= '';
+
+  async function handleSignUp() {
+    if(!name || !email || !password){
+      return //send toastr error.
+    }
+
+    const response = await fetch(`${import.meta.env.VITE_API_URL}/signup`, {
+      method: "POST",
+      body: JSON.stringify({
+        name, email, password
+      }),
+      headers: {
+        'Content-type': 'application/json'
+      }
+    })
+
+    if(response.ok){
+      return // Toast success
+    } else {
+      return //Toast fail.
+    }
+
+  }
+  
+
+  
+</script>
+
 <section class="gradient-form min-h-screen min-w-screen bg-neutral-200 dark:bg-neutral-700">
   <div class="container h-full p-10">
     <div
@@ -27,6 +59,7 @@
                       class="peer block min-h-[auto] w-full rounded border-0 bg-neutral-1 px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
                       id="exampleFormControlInput1"
                       placeholder="Your Name"
+                      bind:value={name}
                     />
                     <label
                       for="exampleFormControlInput1"
@@ -35,13 +68,14 @@
                     </label>
                   </div>
 
-                  <!--Username input-->
+                  <!--Email input-->
                   <div class="relative mb-4" data-te-input-wrapper-init>
                     <input
-                      type="text"
+                      type="email"
                       class="peer block min-h-[auto] w-full rounded border-0 bg-neutral-1 px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
                       id="exampleFormControlInput1"
                       placeholder="Your Email"
+                      bind:value={email}
                     />
                     <label
                       for="exampleFormControlInput1"
@@ -57,6 +91,7 @@
                       class="peer block min-h-[auto] w-full rounded border-0 bg-neutral-1 px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
                       id="exampleFormControlInput11"
                       placeholder="Password"
+                      bind:value={password}
                     />
                     <label
                       for="exampleFormControlInput11"
@@ -64,37 +99,6 @@
                       >Password
                     </label>
                   </div>
-
-                  <!--Repeat Password input-->
-                  <div class="relative mb-4" data-te-input-wrapper-init>
-                    <input
-                      type="password"
-                      class="peer block min-h-[auto] w-full rounded border-0 bg-neutral-1 px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-                      id="exampleFormControlInput12"
-                      placeholder="Repeat Password"
-                    />
-                    <label
-                      for="exampleFormControlInput12"
-                      class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary-600 peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
-                      >Repeat Password
-                    </label>
-                  </div>
-
-                    <!--Default checkbox-->
-                    <div class="mb-2 block min-h-[1.5rem] pl-[1.5rem]">
-                      <input
-                        class="relative float-left -ml-[1.5rem] mr-[6px] mt-[0.15rem] h-[1.125rem] w-[1.125rem] appearance-none rounded-[0.25rem] border-[0.125rem] border-solid border-neutral-300 outline-none before:pointer-events-none before:absolute before:h-[0.875rem] before:w-[0.875rem] before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-[0px_0px_0px_13px_transparent] before:content-[''] checked:border-primary checked:bg-primary checked:before:opacity-[0.16] checked:after:absolute checked:after:-mt-px checked:after:ml-[0.25rem] checked:after:block checked:after:h-[0.8125rem] checked:after:w-[0.375rem] checked:after:rotate-45 checked:after:border-[0.125rem] checked:after:border-l-0 checked:after:border-t-0 checked:after:border-solid checked:after:border-white checked:after:bg-transparent checked:after:content-[''] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:shadow-none focus:transition-[border-color_0.2s] focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-[0.875rem] focus:after:w-[0.875rem] focus:after:rounded-[0.125rem] focus:after:content-[''] checked:focus:before:scale-100 checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:after:-mt-px checked:focus:after:ml-[0.25rem] checked:focus:after:h-[0.8125rem] checked:focus:after:w-[0.375rem] checked:focus:after:rotate-45 checked:focus:after:rounded-none checked:focus:after:border-[0.125rem] checked:focus:after:border-l-0 checked:focus:after:border-t-0 checked:focus:after:border-solid checked:focus:after:border-white checked:focus:after:bg-transparent dark:border-neutral-600 dark:checked:border-primary dark:checked:bg-primary dark:focus:before:shadow-[0px_0px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca]"
-                        type="checkbox"
-                        value=""
-                        id="checkboxDefault"
-                      />
-                      <label
-                        class="inline-block pl-[0.15rem] hover:cursor-pointer"
-                        for="checkboxDefault"
-                      >
-                        I agree all statements in <a href="#!">Terms of service</a>
-                      </label>
-                    </div>
                      <!--Submit button-->
                   <div class="mb-12 pb-1 pt-1 text-center">
                     <button
@@ -105,6 +109,7 @@
                       style="
                           background: linear-gradient(to left, #ff499e, #d264b6, #a480cf, #779be7, #49b6ff);
                         "
+                      on:click={handleSignUp}
                     >
                       Sign Up
                     </button>
