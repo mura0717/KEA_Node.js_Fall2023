9ee4f1ba873406b2db9435c65b8c3c33f22530e7
mura0717 <mura0717@stud.kea.dk>
2023-11-11 01:55:22 +0100

    contact form added

--- 11._Mandatory_II/client/src/pages/UserProfile/UserProfile.svelte
+++ 11._Mandatory_II/client/src/pages/UserProfile/UserProfile.svelte
@@ -10,7 +10,7 @@
 </script>
 
 <section
-  class="gradient-form min-h-screen min-w-screen bg-neutral-200 dark:bg-neutral-700"
+  class="gradient-form bg-neutral-200 dark:bg-neutral-700"
 >
   <div class="container h-full p-10">
     <div
@@ -23,14 +23,36 @@
               class="container min-h-screen min-w-screen my-12 mx-auto md:px-96"
             >
               <section class="mb-32 text-center">
-                <h2 class="mb-12 text-3xl font-bold outline-none">
-                  {userName}'s Profile Page
+                <h2 class="mb-3 text-3xl font-bold outline-none">
+                  Welcome " {userName} "
                 </h2>
-                  <div class="mb-6 p-6">
-                    <div class="relative overflow-hidden bg-cover bg-no-repeat">
-                      <h5 class="mb-4 text-lg font-bold">My Reminders:</h5>
+                <h5 class="mb-12 outline-none">
+                  Use the form below to contact us.
+                </h5>
+                <div class="container mx-auto md:px-6">
+                  <section class="mb-32 text-center">
+                    <div class="mx-auto max-w-[700px] md:px-12 flex flex-col justify-center items-center h-full">
+                      <form class="w-full">
+                        <div class="relative mb-6" data-te-input-wrapper-init>
+                          <textarea
+                            class="peer block min-h-[auto] ml-[-170px] bg-neutral-200 dark:bg-neutral-700 rounded border-0 bg-transparent py-[0.32rem] px-6 leading-[1.6] outline-none"                            id="text-message"
+                            rows="6"
+                            placeholder="Your message"
+                            style="width: 400%; padding: 15px;"
+                          />
+                        </div>
+                        <button
+                          type="button"
+                          data-te-ripple-init
+                          data-te-ripple-color="light"
+                          class="mb-6 inline-block w-full rounded bg-neutral-700 px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+                        >
+                          Send
+                        </button>
+                      </form>
                     </div>
-                  </div>
+                  </section>
+                </div>
               </section>
             </div>
           </div>
